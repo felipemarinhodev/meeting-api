113f1d35b925a2ca4f3fc9801ad7757aee73b3d0
Felipe Marinho <felipmarinho@gmail.com>
2019-09-29 05:45:00 -0300

    Adding auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,12 @@ class UserController {
     const { id, name, email } = await User.create(req.body);
     return res.json({ id, name, email });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
