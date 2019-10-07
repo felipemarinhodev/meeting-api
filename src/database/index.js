4256c3bd5849065b7cf8340ace2ac9cbd76f8964
Felipe Marinho <felipmarinho@gmail.com>
2019-10-07 05:02:05 -0300

    Adding mongoose and configuring for to use mongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -12,6 +13,7 @@ const models = [User, File, Appointment, Room];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   //  Responsável por conectar com a base de dados e carregar os models.
@@ -21,6 +23,18 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose
+      .connect('mongodb://localhost:27017/meeting', {
+        useNewUrlParser: true, // Pois estou utilizando uma url em um formato mais novo.
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('DB Connected!'))
+      .catch(err => {
+        console.log(`DB Connection Error: ${err.message}`);
+      });
   }
 }
 
